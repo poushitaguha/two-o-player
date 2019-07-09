ac824f0c8103c1c8ac07196a9094968921ba3fee
poushitaguha <pguha.kolkata@gmail.com>
2019-07-09 00:20:39 +0000

    Did indentation

--- turns.rb
+++ turns.rb
@@ -39,17 +39,17 @@ class Turns
             end
         
             if @player1.lives == 0 || @player2.lives == 0
-            @game.gameover = true
-            if @player1.lives == 0
-                puts "Player 2 wins with a score of #{@player2.lives}/3"
-            else
-                puts "Player 1 wins with a score of #{@player1.lives}/3"
-            end
+                @game.gameover = true
+                if @player1.lives == 0
+                    puts "Player 2 wins with a score of #{@player2.lives}/3"
+                else
+                    puts "Player 1 wins with a score of #{@player1.lives}/3"
+                end
             end
         
             if @game.currentPlayer == 1
                 @game.currentPlayer = 2
-                else
+            else
                 @game.currentPlayer = 1
             end
         
